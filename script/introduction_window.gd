69d3f8d16135fdae5e6c2d63d0c1208ae423374d
Nivdc <nivdc@live.com>
2023-10-01 16:49:36 +0800

    add buy&sell GUI table

--- script/introduction_window.gd
+++ script/introduction_window.gd
@@ -2,17 +2,11 @@ extends Window
 
 signal game_core_readied
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	self.set_process(false)#禁用帧处理，不需要这个
 	self.connect("close_requested",func():self.hide())
 	self.connect("game_core_readied",func():$Button.set_disabled(false))
 	$Button.pressed.connect(self._start_game)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
 
 func _start_game():
--- script/main.gd
+++ script/main.gd
@@ -4,21 +4,35 @@ signal game_core_readied
 signal game_started
 var core = null
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	randomize()#初始化随机数种子
 	self.set_process(false)#禁用帧处理，不需要这个
+	# $MainContainer.hide()#暂时隐藏主窗口内容，等待游戏开始
 
 	# 搞了半天你跟我说主窗口不能隐藏？...行，我忍了，这个也不是很重要吧。
 	# self.get_window().hide()
 	# self.connect("game_started",func():self.get_window().show())
+	self.connect("game_started",func():$MainContainer.show())
 
 	self.connect("game_core_readied",func():$IntroWindow.emit_signal("game_core_readied"))
 	core = preload("res://script/core/game_core.gd").new(self)
 
-	pass # Replace with function body.
+	update_tree()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func update_tree():
+	var good_tree = $MainContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Tree
+	good_tree.clear()
+	good_tree.set_column_title(0, "商品")
+	good_tree.set_column_title(1, "黑市价格")
+	good_tree.set_column_title_alignment(0, HORIZONTAL_ALIGNMENT_LEFT)
+	good_tree.set_column_title_alignment(1, HORIZONTAL_ALIGNMENT_LEFT)
+	var root = good_tree.create_item()
+	for good in core.goods_list:
+		if good["is_active"] == true:
+			var good_treeitem = good_tree.create_item(root)
+			good_treeitem.set_text(0, good["name"])
+			good_treeitem.set_text(1, str(good["price"]))
+			good_treeitem.set_tooltip_text(0, " ")
+			good_treeitem.set_tooltip_text(1, " ")
