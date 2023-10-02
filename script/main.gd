dfe2836faaab6e69e37d006574f2adbdbcab39f5
Nivdc <nivdc@live.com>
2023-10-02 15:51:22 +0800

    finish main scene layout

--- script/main.gd
+++ script/main.gd
@@ -18,21 +18,43 @@ func _ready():
 	self.connect("game_core_readied",func():$IntroWindow.emit_signal("game_core_readied"))
 	core = preload("res://script/core/game_core.gd").new(self)
 
-	update_tree()
+	core.add_good("《上海小宝贝》（禁书）",100)
+	update_trees()
 
 
-func update_tree():
+func update_trees():
 	var good_tree = $MainContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Tree
 	good_tree.clear()
 	good_tree.set_column_title(0, "商品")
 	good_tree.set_column_title(1, "黑市价格")
 	good_tree.set_column_title_alignment(0, HORIZONTAL_ALIGNMENT_LEFT)
 	good_tree.set_column_title_alignment(1, HORIZONTAL_ALIGNMENT_LEFT)
-	var root = good_tree.create_item()
+	var good_tree_root = good_tree.create_item()
 	for good in core.goods_list:
 		if good["is_active"] == true:
-			var good_treeitem = good_tree.create_item(root)
+			var good_treeitem = good_tree.create_item(good_tree_root)
 			good_treeitem.set_text(0, good["name"])
 			good_treeitem.set_text(1, str(good["price"]))
 			good_treeitem.set_tooltip_text(0, " ")
 			good_treeitem.set_tooltip_text(1, " ")
+
+	var storage_tree_title_label = $MainContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer3/Label
+	storage_tree_title_label.set_text("您的出租屋( %d/%d )" % [core.player_status["used_storage_size"], core.player_status["storage_size"]])
+	var storage_tree = $MainContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer3/Tree
+	storage_tree.clear()
+	storage_tree.set_column_title(0, "商品")
+	storage_tree.set_column_title(1, "买进价格")
+	storage_tree.set_column_title(2, "数量")
+	storage_tree.set_column_title_alignment(0, HORIZONTAL_ALIGNMENT_LEFT)
+	storage_tree.set_column_title_alignment(1, HORIZONTAL_ALIGNMENT_LEFT)
+	storage_tree.set_column_title_alignment(2, HORIZONTAL_ALIGNMENT_LEFT)
+	# storage_tree.set_column_custom_minimum_width （这一行留待后用)
+	var storage_tree_root = storage_tree.create_item()
+	for good_name in core.player_status["storage"].keys():
+		var storage_treeitem = storage_tree.create_item(storage_tree_root)
+		storage_treeitem.set_text(0, good_name)
+		storage_treeitem.set_text(1, str(core.player_status["storage"][good_name]["record_price"]))
+		storage_treeitem.set_text(2, str(core.player_status["storage"][good_name]["number"]))
+		storage_treeitem.set_tooltip_text(0, " ")
+		storage_treeitem.set_tooltip_text(1, " ")
+		storage_treeitem.set_tooltip_text(2, " ")
